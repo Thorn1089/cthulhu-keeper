3a744760117437b7b17c9204e21681244bb6a95d
Tom Golden <TomRK1089@gmail.com>
2020-01-30 22:35:27 -0500

    Generates XML test report for Jest for Sonar to pick up

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/cypress']
+  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/cypress'],
+  testResultsProcessor: 'jest-sonar-reporter'
 };
